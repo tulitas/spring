233f1c27e3a453103896592d432a3056d81daf31
SergejK <tulitas1@gmail.com>
2020-06-12 23:56:46 +0300

    not ended registartion controller

--- src/main/java/com/spring/config/MvcConfig.java
+++ src/main/java/com/spring/config/MvcConfig.java
@@ -1,4 +1,12 @@
 package com.spring.config;
 
-public class MvcConfig {
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/news").setViewName("news");
+    }
 }
--- src/main/java/com/spring/controller/RegistrationController.java
+++ src/main/java/com/spring/controller/RegistrationController.java
@@ -1,4 +1,23 @@
 package com.spring.controller;
 
+import com.spring.entity.User;
+import com.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+
 public class RegistrationController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+        return "registration";
+    }
+
 }
